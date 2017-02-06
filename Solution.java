5bfe1840e52989ca396f6ea70f41ccee81f5e63b
Oleg <okan2@UKR.NET>
2017-02-06 20:15:53 +0200

    1st method

---
+++ Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.test;
+
+/**
+ * Created by Oleg on 2/6/2017.
+ */
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Поцелуй мой блестящий металлический зад!");
+    }
+}
